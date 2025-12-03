92212e8b38c76502a8b6c7d4b818d4e3c040f2be
MRNaatey <petertuiter1@gmail.com>
2025-12-03 22:57:31 +0000

    Update Chapter1.hs

--- Pratical-Task/Chapter1.hs
+++ Pratical-Task/Chapter1.hs
@@ -1,117 +1,114 @@
-double :: Int -> Int -> Int
-double x y = x * y * 2
- 
-main :: IO ()
-main = do
- print $ double 8 9
---output is = 144 
+import Data.List (sortBy)
+import Data.Ord (comparing)
 
-increment :: Int -> Int
-increment x = x + 1
+------------------------------------------------------------
+-- HC1T1 – Task 1: Function Composition
+------------------------------------------------------------
 
-main :: IO ()
-main = do
- print $ increment 5
- --output is = 6
-
+-- double multiplies a number by 2
 double :: Int -> Int
 double x = x * 2
 
+-- increment increases a number by 1
 increment :: Int -> Int
 increment x = x + 1
 
-doubleThenincrement :: Int -> Int
-doubleThenincrement x = increment (double x)
-  where 
-    double n = n * 2
-    increment n = n + 1
-  
-main :: IO ()
-main = do
- print $ doubleThenincrement 6
- --output is 13
+-- doubleThenIncrement applies double first and then increment
+doubleThenIncrement :: Int -> Int
+doubleThenIncrement x = increment (double x)
+
+
+------------------------------------------------------------
+-- HC1T2 – Task 2: Pure Function Example
+------------------------------------------------------------
 
 circleArea :: Double -> Double
-circleArea r = area
-  where
-    area = pi * r * r
+circleArea r = pi * r * r
 
-main :: IO ()
-main = do
-  print (circleArea 9)
-  --output is = 254.46900494077323
+------------------------------------------------------------
+-- HC1T3 – Task 3: Checking if a Number is Greater than 18
+------------------------------------------------------------
 
 greaterThan18 :: Int -> Bool
 greaterThan18 x = x > 18
 
-main :: IO()
-main = do
- print (greaterThan18 17)
- --output is = false
- import Data.List (sortBy)
-import Data.Ord (comparing)
+------------------------------------------------------------
+-- HC1T4 – Task 4: Composing a Function to Process Player Data
+------------------------------------------------------------
 
--- Extract player names from a list of (name, score) tuples
 extractPlayers :: [(String, Int)] -> [String]
 extractPlayers players = [name | (name, _) <- players]
 
--- Sort the list of players by score in descending order
 sortByScore :: [(String, Int)] -> [(String, Int)]
 sortByScore players = sortBy (comparing (negate . snd)) players
 
--- Return the top three players (name and score)
 topThree :: [(String, Int)] -> [(String, Int)]
 topThree players = take 3 players
 
--- Compose all functions to get the top three player names
 getTopThreePlayers :: [(String, Int)] -> [String]
 getTopThreePlayers = extractPlayers . topThree . sortByScore
 
--- Main function to test getTopThreePlayers
-main :: IO ()
-main = do
-  let playerData = [("Harding", 50), ("Prince", 75), ("Calvin", 100), ("Diana", 90)]
-  print (getTopThreePlayers playerData)  
-  --output is = ["Calvin","Diana","Prince"]
- 
- --HCIT5 create a function that extract the first x terms from an infinite list
-giveMe :: Int -> [Int]
-giveMe x = take x [1..]
+------------------------------------------------------------
+-- HC1T5 – Task 5: Laziness in Haskell
+------------------------------------------------------------
 
-main :: IO ()
-main = do
-  print (giveMe 7) 
-  -- output is = [1,2,3,4,5,6,7]
-  
- -- HCIT 6 Takes two integers and returns their sum
+infiniteNumbers :: [Int]
+infiniteNumbers = [1..]
+
+firstNNumbers :: Int -> [Int]
+firstNNumbers n = take n infiniteNumbers
+
+------------------------------------------------------------
+-- HC1T6 – Task 6: Using Type Signatures
+------------------------------------------------------------
+
 addNumbers :: Int -> Int -> Int
 addNumbers x y = x + y
 
-main :: IO ()
-main = do
- print (addNumbers 19 17)
--- output is = 36
--- HC1T7 A function that takes the temperature in Fahrenheit and returns it in Celsius
+------------------------------------------------------------
+-- HC1T7 – Task 7: Converting Fahrenheit to Celsius
+------------------------------------------------------------
+
+fToC :: Double -> Double
 fToC x = (x - 32) * 5 / 9
 
-main :: IO ()
-main = do 
- print (fToC 212)
- --output is = 100.00
- --HC1T8 applytwice Increment function (adds 8)
-increment :: Int -> Int
-increment x = x + 8
+------------------------------------------------------------
+-- HC1T8 – Task 8: Higher-Order Functions
+------------------------------------------------------------
 
--- applyTwice applies a function twice to a value
+incrementBy8 :: Int -> Int
+incrementBy8 x = x + 8
+
 applyTwice :: (a -> a) -> a -> a
 applyTwice f x = f (f x)
 
+------------------------------------------------------------
+-- Main function testing all tasks
+------------------------------------------------------------
+
 main :: IO ()
 main = do
-  print (applyTwice increment 5)  
-  --output is = 21
+  -- HC1T1
+  print (doubleThenIncrement 6)       
 
+  -- HC1T2
+  print (circleArea 9)                 
 
+  -- HC1T3
+  print (greaterThan18 17)            
 
+  -- HC1T4
+  let playerData = [("Harding",50),("Prince",75),("Calvin",100),("Diana",90)]
+  print (getTopThreePlayers playerData)
 
+  -- HC1T5
+  print (firstNNumbers 7)              
 
+  -- HC1T6
+  print (addNumbers 19 17)             
+
+  -- HC1T7
+  print (fToC 212)                     
+
+  -- HC1T8
+  print (applyTwice incrementBy8 5)    
